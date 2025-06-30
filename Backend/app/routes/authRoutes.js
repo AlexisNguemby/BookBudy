26232cacb8b8656a20559c217cc2fe9493f1de77
sebastien-liveyupeng <sebastien.live-yu-peng@laplateforme.io>
2025-06-30 13:44:47 +0200

    adjusting path for require controller

--- Backend/app/routes/authRoutes.js
+++ Backend/app/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, logout, getProfile, updateProfile } = require('../controllers/authController');
+const { register, login, logout, getProfile, updateProfile } = require('../controllers/AuthController');
 const { protect } = require('../middlewares/authMiddleware');
 
 router.post('/register', register);
--- Backend/app/routes/bookRoutes.js
+++ Backend/app/routes/bookRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const { protect } = require('../middlewares/authMiddleware');
-const { addBookToCollection, getUserBooks, updateBook } = require('../controllers/BookController');
+const { addBookToCollection, getUserBooks, updateBook } = require('../controllers/bookController');
 
 router.post('/', protect, addBookToCollection);
 router.get('/', protect, getUserBooks); // get user books
